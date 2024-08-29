e4909955926e96c20ba06b489e0865349d7de885
GaboGavo <grherrera8@misena.edu.co>
2024-08-29 16:09:09 -0500

    Test unitarios usecase y categoryrestcontroller, categoryAdapter, sonarlint

--- src/main/java/com/bootcampPragma/EmazonTest01/adapters/driven/jpa/mysql/exception/CategoryNotFoundException.java
+++ src/main/java/com/bootcampPragma/EmazonTest01/adapters/driven/jpa/mysql/exception/CategoryNotFoundException.java
@@ -1,9 +1,6 @@
 package com.bootcampPragma.EmazonTest01.adapters.driven.jpa.mysql.exception;
 
 public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException() {
-        super("Category not found");
-    }
 
     public CategoryNotFoundException(String message) {
         super(message);
